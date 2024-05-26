a936adc09ed43095bc67727f9cf242591d30bace
Hataewoo <teawoo163@gmail.com>
2024-05-26 18:03:30 +0900

    Game Framework

---
+++ src/Scenes/MainScene.py
@@ -0,0 +1,11 @@
+from GameFramework import Scene
+
+class MainScene(Scene) :
+    def Setup() :
+        print("Setup")
+
+    def Update() :
+        print("Update")
+
+    def Exit() :
+        print("Exit")
